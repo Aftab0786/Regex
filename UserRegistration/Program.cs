cf84d59b798b9b868ae4813d86eb3ccad39ef977
Aftab-1998 <alam41976@gmail.com>
2022-02-11 11:59:20 +0530

    [AFTAB4] Add; Added the mobile number sample is executed

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -6,7 +6,8 @@ class program
         Pattern pattern = new Pattern();
         //Console.WriteLine(pattern.validateFirstName("Raj"));
         //Console.WriteLine(pattern.validateLastName("Roy"));
-        Console.WriteLine(pattern.valiadtaeEmail("alam41976@gmail.com"));
+        //Console.WriteLine(pattern.valiadtaeEmail("alam41976@gmail.com"));     
+        Console.WriteLine(pattern.validateMobileNumber("91 79719623"));
     }
 
 }
